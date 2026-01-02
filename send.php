9d5c5d7831d3de7a650b451f4116a321c4cbfd73
Andrey Malinin <andreygwa@gmail.com>
2026-01-02 15:56:25 +0300

    Send.php

---
+++ send.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $to = "andreyrasp@outlook.com"; // email
+    $subject = "Новая заявка Mersin Healing";
+
+    $message = "
+Имя: {$_POST['name']}
+Возраст: {$_POST['age']}
+Проблемы: " . implode(", ", (array)$_POST['problems']) . "
+Комментарий: {$_POST['problem_notes']}
+
+Вес: {$_POST['weight']}
+Рост: {$_POST['height']}
+Талия: {$_POST['waist']}
+
+Проживание: {$_POST['accommodation']}
+Способ связи: {$_POST['contact_method']}
+Контакты: {$_POST['contact_details']}
+
+Время на восстановление: {$_POST['healing_time']}
+Даты: {$_POST['arrival_date']} — {$_POST['departure_date']}
+";
+
+    $headers = "From: no-reply@mersin-healing.com\r\n";
+
+    mail($to, $subject, $message, $headers);
+
+    echo "OK";
+}
+?>
